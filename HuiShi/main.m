ba12b71c7f2a92c30ec26bdd8308ed4c4e492a1c
VictorWang2014 <761714330@qq.com>
2016-07-11 22:44:39 +0800

    Initial Commit

---
+++ HuiShi/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  HuiShi
+//
+//  Created by admin on 16/7/11.
+//  Copyright © 2016年 DiZiCompanyLimited. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
